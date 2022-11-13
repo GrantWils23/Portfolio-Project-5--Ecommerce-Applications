48a7144f875d97925d442b1bb0074d2036e61ca5
Grant <GrantWils.23@googlemail.com>
2022-11-13 22:34:01 +0000

    added filter by brands

--- products/views.py
+++ products/views.py
@@ -9,21 +9,21 @@ from .models import Product, Category, Brand
 def all_products(request):
     """ A view to show all products, includingsorting and search queries """
 
+    brands = Brand.objects.all()
     products = Product.objects.all()
     query = None
     categories = None
-    brands = None
 
     if request.GET:
-        # if 'brand' in request.GET:
-        #     brands = request.GET['brand'].split(',')
-        #     products = products.filter(brand__name__in=brands)
-        #     brands = Brand.objects.filter(name__in=brands)
-
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
+ 
+        if 'brand' in request.GET:
+            brands = request.GET['brand'].split(',')
+            products = products.filter(brand__name__in=brands)
+            brands = Brand.objects.filter(name__in=brands)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -38,7 +38,7 @@ def all_products(request):
         'products': products,
         'search_term': query,
         'current_categories': categories,
-        # 'current_brands': brands
+        'current_brands': brands,
     }
 
     return render(request, 'products/products.html', context)
