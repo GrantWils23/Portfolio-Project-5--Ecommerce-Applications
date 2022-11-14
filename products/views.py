73ca33200b9f581899b0f27cf4a439c4fa70dfaa
Grant <GrantWils.23@googlemail.com>
2022-11-14 15:52:33 +0000

    added sorting JS to the products.html

--- products/views.py
+++ products/views.py
@@ -27,6 +27,8 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
+            if sortkey == 'brand':
+                sortkey = 'brand__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == "desc":
