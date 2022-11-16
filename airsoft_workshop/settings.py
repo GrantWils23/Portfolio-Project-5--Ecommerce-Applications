647ed6e965103feb497226e7a878250f4b470314
Grant <GrantWils.23@googlemail.com>
2022-11-16 13:14:42 +0000

    Added Notification CSS, shopping bag preview and additional messages

--- airsoft_workshop/settings.py
+++ airsoft_workshop/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-grantwils23-portfoliopr-7o2kyngu7uz.ws-eu74.gitpod.io']
-# CSRF_TRUSTED_ORIGINS = ['https://8000-grantwils23-portfoliopr-7o2kyngu7uz.ws-eu75.gitpod.io']
+# CSRF_TRUSTED_ORIGINS = ['https://8000-grantwils23-portfoliopr-7o2kyngu7uz.ws-eu74.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://8000-grantwils23-portfoliopr-7o2kyngu7uz.ws-eu75.gitpod.io']
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -14,17 +14,21 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the basket """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
-        # messages.success(request, f'Updated {product.name} to quantity to {}')
+        messages.success(
+            request, f'Updated {product.name}'
+            f' quantity to {basket[item_id]}'
+            )
     else:
         basket[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your basket')
+        messages.success(request, f' x {basket[item_id]}'
+                         f' {product.name} has been added to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
@@ -33,13 +37,19 @@ def add_to_basket(request, item_id):
 def adjust_basket(request, item_id):
     """ Adjust the qty of the specified product to the specified amount """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     basket = request.session.get('basket', {})
 
     if quantity > 0:
         basket[item_id] = quantity
+        messages.info(
+            request, f'Updated {product.name}'
+            f' quantity to {basket[item_id]}'
+            )
     else:
         basket.pop(item_id)
+        messages.info(request, f'Removed {product.name} from your basket')
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
@@ -49,11 +59,15 @@ def remove_from_basket(request, item_id):
     """ Remove the item from the shopping basket """
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         basket = request.session.get('basket', {})
 
         basket.pop(item_id)
+        messages.warning(request, f'Removed {product.name} from your basket')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
+
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
