1552e32515b02aba7593b6f4272a64199a0c69d5
Grant <GrantWils.23@googlemail.com>
2022-12-03 15:55:23 +0000

    set debug to flase

--- airsoft_workshop/settings.py
+++ airsoft_workshop/settings.py
@@ -36,8 +36,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = 'True'
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'False'
 
 ALLOWED_HOSTS = ['airsoft-workshop.herokuapp.com', '127.0.0.1', 'localhost']
 
